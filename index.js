5b1475346965b81eca6b9f038a20153096a08b79
Alamin-Ahammed <ahammedalamin2002@gmail.com>
2024-01-21 11:57:59 +0600

    added sound effects and more game instractions

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const shootingSound = new Audio("./audios/laser-gun.mp3");
+const playerDeath = new Audio("./audios/player-death-explosion.mp3");
+const megaSound = new Audio("./audios/spaceship-cannon-load-shoot.mp3");
 class SpaceShooterGame {
   constructor() {
     this.playBtn = document.querySelector(".playbtn");
@@ -7,8 +11,10 @@ class SpaceShooterGame {
     this.bulletsContainer = document.querySelector(".bullets");
     this.enemiesContainer = document.querySelector(".enemies");
     this.points = document.querySelector(".points");
+    this.titleContainer = document.querySelector(".titleContainer");
     this.showEnemyInterval = null;
     this.destroyEnemyInterval = null;
+    this.isMuted = false;
 
     this.playBtn.addEventListener("click", () => this.startGame());
     this.stopBtn.addEventListener("click", () => this.stopGame());
@@ -19,6 +25,8 @@ class SpaceShooterGame {
   startGame() {
     this.stopBtn.style.display = "inline-block";
     this.playBtn.style.display = "none";
+    this.titleContainer.style.display = "none";
+    audio.play();
 
     this.playground.addEventListener("mousemove", (e) => this.moveRocket(e));
     this.showEnemyInterval = setInterval(() => this.createEnemy(), 3000);
@@ -26,7 +34,7 @@ class SpaceShooterGame {
   }
 
   stopGame() {
-    this.enemiesContainer.innerHTML = '';
+    this.enemiesContainer.innerHTML = "";
     this.stopBtn.style.display = "none";
     this.playBtn.style.display = "inline-block";
     this.playground.removeEventListener("mousemove", (e) => this.moveRocket(e));
@@ -46,7 +54,20 @@ class SpaceShooterGame {
   }
 
   fireBullet(e) {
-    console.log(e)
+    if (!this.isMuted) {
+      this.clearShootingSoundInterval = setInterval(
+        () => shootingSound.play(),
+        30
+      );
+    }
+
+    onkeydown = (e) => {
+      if (e.key === "s") {
+        console.log("S is pressed");
+        this.isMuted = true;
+        clearInterval(this.clearShootingSoundInterval);
+      }
+    };
     if (e.button === 1) {
       this.fireingInterval = setInterval(() => this.createBullet(), 400);
     }
@@ -55,6 +76,7 @@ class SpaceShooterGame {
   stopFire() {
     this.bulletsContainer.innerHTML = "";
     clearInterval(this.fireingInterval);
+    clearInterval(this.clearShootingSoundInterval);
   }
 
   createBullet() {
@@ -64,7 +86,9 @@ class SpaceShooterGame {
     this.bulletsContainer.appendChild(newBullet);
 
     if (this.bulletsContainer.children.length > 3) {
-      this.bulletsContainer.removeChild(this.bulletsContainer.firstElementChild);
+      this.bulletsContainer.removeChild(
+        this.bulletsContainer.firstElementChild
+      );
     }
   }
 
@@ -77,13 +101,13 @@ class SpaceShooterGame {
   }
 
   destroyEnemy() {
-    let bullets = this.bulletsContainer.querySelectorAll('.bullet');
-    let enemies = this.enemiesContainer.querySelectorAll('.enemy');
+    let bullets = this.bulletsContainer.querySelectorAll(".bullet");
+    let enemies = this.enemiesContainer.querySelectorAll(".enemy");
 
-    bullets.forEach(bullet => {
+    bullets.forEach((bullet) => {
       let bulletRect = bullet.getBoundingClientRect();
 
-      enemies.forEach(enemy => {
+      enemies.forEach((enemy) => {
         let enemyRect = enemy.getBoundingClientRect();
 
         if (this.isCollision(bulletRect, enemyRect)) {
@@ -97,14 +121,13 @@ class SpaceShooterGame {
 
   isCollision(rect1, rect2) {
     // rect1 is the bulletRect  &&  rect2 is the enemyRect
-    return rect1.x < rect2.x + rect2.width &&
+    return (
+      rect1.x < rect2.x + rect2.width &&
       rect1.x + rect1.width > rect2.x &&
       rect1.y < rect2.y + rect2.height &&
-      rect1.y + rect1.height > rect2.y;
+      rect1.y + rect1.height > rect2.y
+    );
   }
 }
 
-
 const spaceShooterGame = new SpaceShooterGame();
-
-
